bee2e2b5ae93d16e66a587cd95d1c5e3ead13dc6
mr687 <dapino.dev@gmail.com>
2023-01-23 06:47:22 +0700

    add probability score

--- main.py
+++ main.py
@@ -21,25 +21,35 @@ def load_vectorizer():
 		vectorizer = pkl.load(f)
 		return vectorizer
 
+float_to_percent = lambda x: f"{x*100:.1f}%"
+
 def get_prediction(tweet_text):
 	model = load_model()
 	vectorizer = load_vectorizer()
 	tweet_vector = vectorizer.transform([tweet_text])
 	subjectivity = model.predict(tweet_vector)
-	if subjectivity == 0:
-		return 'Negative'
-	else:
-		return 'Positive'
+	probabilities = model.predict_proba(tweet_vector)
+	return subjectivity, probabilities
 
 def write_result_sentiment(st, result):
-	if result == 'Negative':
-		st.error(f"Sentiment: {result}")
+	subjectivity, probabilities = result
+	negative_proba = probabilities[0][0]
+	positive_proba = probabilities[0][1]
+	sentiment = 'Negative' if subjectivity[0] == 0 else 'Positive'
+	
+	st.info(f"""
+	Polarity score
+	- Negative score: `{negative_proba}` ({float_to_percent(negative_proba)})
+	- Positive score: `{positive_proba}` ({float_to_percent(positive_proba)})
+	""")
+	if sentiment == 'Negative':
+		st.error(f"Sentiment: `{sentiment}`")
 	else:
-		st.success(f"Sentiment: {result}")
+		st.success(f"Sentiment: `{sentiment}`")
 
 def write_result(st, text_ori, text_clean, result):
 	st.write(f"Tweet Original: `{text_ori}`")
-	st.write(f"Preprocessed Tweet: `{text_clean}`")
+	# st.write(f"Preprocessed Tweet: `{text_clean}`")
 	write_result_sentiment(st, result)
 
 st.title("Sentiment Analysis of Tweets about Indonesia's Fuel Price Hike")
@@ -53,27 +63,27 @@ with st.expander('Manual Input'):
 		write_result(st, tweet_text, text, result)
 		st.balloons()
 
-with st.expander('Upload File (CSV)'):
-	file = st.file_uploader('Upload File', type=['csv'])
-	delim = st.text_input('Delimiter', value=',', max_chars=1)
-	column = st.text_input('Column Name', value='text')
-	if st.button('Analyze', key='upload'):
-		df = pd.read_csv(file, delimiter=delim)
-		data_count = len(df)
+# with st.expander('Upload File (CSV)'):
+# 	file = st.file_uploader('Upload File', type=['csv'])
+# 	delim = st.text_input('Delimiter', value=',', max_chars=1)
+# 	column = st.text_input('Column Name', value='text')
+# 	if st.button('Analyze', key='upload'):
+# 		df = pd.read_csv(file, delimiter=delim)
+# 		data_count = len(df)
 
-		with st.spinner('Wait for it...'):
-			progress_bar = st.progress(0)
-			df['text_clean'] = df[column].swifter.apply(preprocess_text)
-			progress_bar.progress(data_count * 0.5)
-			df['sentiment'] = df['text_clean'].swifter.apply(get_prediction)
-			progress_bar.progress(100)
-			st.write(df[[column, 'sentiment']])
+# 		with st.spinner('Wait for it...'):
+# 			progress_bar = st.progress(0)
+# 			df['text_clean'] = df[column].swifter.apply(preprocess_text)
+# 			progress_bar.progress(data_count * 0.5)
+# 			df['sentiment'] = df['text_clean'].swifter.apply(get_prediction)
+# 			progress_bar.progress(100)
+# 			st.write(df[[column, 'sentiment']])
 		
-		st.balloons()
+# 		st.balloons()
 
-		st.download_button(
-			label='Download Result',
-			data=df.to_csv(index=False).encode('utf-8'),
-			file_name=f'{file.name}-result.csv',
-			mime='text/csv'
-		)
+# 		st.download_button(
+# 			label='Download Result',
+# 			data=df.to_csv(index=False).encode('utf-8'),
+# 			file_name=f'{file.name}-result.csv',
+# 			mime='text/csv'
+# 		)
