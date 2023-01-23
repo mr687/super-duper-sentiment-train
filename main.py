71d4830eebd316d89bbe830a9e61c533e09d3de3
mr687 <dapino.dev@gmail.com>
2023-01-24 04:31:59 +0700

    more performa improvements

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ nltk.download('stopwords')
 
 from shared import *
 
+def error_response(st, message):
+	st.error(message)
+
+def success_response(st, message):
+	st.success(message)
+
+def info_response(st, message):
+	st.info(message)
+
 def load_model():
+
 	with open('dataset/model/sental_model.pkl', 'rb') as f:
 		model = pkl.load(f)
 		return model
@@ -23,68 +33,98 @@ def load_vectorizer():
 
 float_to_percent = lambda x: f"{x*100:.1f}%"
 
+model = load_model()
+vectorizer = load_vectorizer()
 def get_prediction(tweet_text):
-	model = load_model()
-	vectorizer = load_vectorizer()
 	tweet_vector = vectorizer.transform([tweet_text])
 	subjectivity = model.predict(tweet_vector)
 	probabilities = model.predict_proba(tweet_vector)
 	return subjectivity, probabilities
 
+def determine_sentiment(subjectivity):
+	return 'Negative' if subjectivity[0] == 0 else 'Positive'
+
 def write_result(st, text_ori, text_clean, result):
 	subjectivity, probabilities = result
 	negative_proba = probabilities[0][0]
 	positive_proba = probabilities[0][1]
-	sentiment = 'Negative' if subjectivity[0] == 0 else 'Positive'
+	sentiment = determine_sentiment(subjectivity)
 	
-	st.info(f"""
+	info_response(st, (f"""
 	Text Preprocessing
 	- Original Text: `{text_ori}`
 	- Preprocessed Text: `{text_clean}`
-	""")
+	"""))
 	
-	st.info(f"""
+	info_response(st, (f"""
 	Polarity score
 	- Negative score: `{negative_proba}` ({float_to_percent(negative_proba)})
 	- Positive score: `{positive_proba}` ({float_to_percent(positive_proba)})
-	""")
+	"""))
 
 	if sentiment == 'Negative':
-		st.error(f"Sentiment: `{sentiment}`")
+		error_response(st, f"Sentiment: `{sentiment}`")
 	else:
-		st.success(f"Sentiment: `{sentiment}`")
+		success_response(st, f"Sentiment: `{sentiment}`")
 
 st.title("Sentiment Analysis of Tweets about Indonesia's Fuel Price Hike")
-st.write("This is a simple sentiment analysis of tweets about Indonesia's fuel price hike developed by [Davi Nomoeh Dani](https://mr687.github.io). The model is trained using a SVM classifier.")
+st.write("This is a simple sentiment analysis of tweets about Indonesia's fuel price hike developed by [Davi Nomoeh Dani](https://mr687.github.io). The model is trained using a SVM classifier and Lexicon-based approach.")
+st.write("---")
+
+def handle_uploaded_file(st, file=None, delim=',', column='text'):
+	if file is None:
+			error_response(st, 'Please upload a file')
+			return
+	try:
+		df = pd.read_csv(file, delimiter=delim)
+	except Exception as e:
+		error_response(st, 'Error reading the file. Please check the file format and delimiter.')
+		return
+	if column not in df.columns:
+		error_response(st, f"Column '{column}' not found in the file. Available columns: `{', '.join(df.columns)}`")
+		return
+	with st.spinner('Preparing data...'):
+		df = df[[column]]
+		total = len(df)
+		info_response(st, f"""
+		Raw Dataset Information
+		- Total Rows: `{total}`
+		- Word Count: `{df[column].swifter.apply(lambda x: len(x.split())).sum()}`
+		""")
+	
+	progress_bar = st.progress(0)
+	with st.spinner('Preprocessing data...'):
+		df = preprocessing(df, column=column)
+		progress_bar.progress(10)
+	with st.spinner('Normalizing data...'):
+		df = normalizer(df, column='tokens')
+		progress_bar.progress(15)
+	with st.spinner('Cleaning data...'):
+		threshold = int(df.shape[0] * 0.1) # threshold is 10% of the total data
+		df = clean_data(df, column='tokens', threshold=threshold)
+		progress_bar.progress(30)
+	with st.spinner('Stemming data...'):
+		df = stemming(df, column='tokens')
+		progress_bar.progress(70)
+	with st.spinner('Predicting data...'):
+		df['subjectivity'], df['probabilities'] = zip(*df['review'].swifter.apply(get_prediction))
+		progress_bar.progress(95)
+		df['sentiment'] = df['subjectivity'].swifter.apply(determine_sentiment)
+		progress_bar.progress(100)
+
+	df = df[[column, 'sentiment']]
+	st.write(df)
 
 with st.expander('Manual Input'):
 	tweet_text = st.text_area('Input Text')
 	if st.button('Analyze'):
-		text = preprocess_text(tweet_text)
+		text = preprocessing(tweet_text)
 		result = get_prediction(text)
 		write_result(st, tweet_text, text, result)
 
-# with st.expander('Upload File (CSV)'):
-# 	file = st.file_uploader('Upload File', type=['csv'])
-# 	delim = st.text_input('Delimiter', value=',', max_chars=1)
-# 	column = st.text_input('Column Name', value='text')
-# 	if st.button('Analyze', key='upload'):
-# 		df = pd.read_csv(file, delimiter=delim)
-# 		data_count = len(df)
-
-# 		with st.spinner('Wait for it...'):
-# 			progress_bar = st.progress(0)
-# 			df['text_clean'] = df[column].swifter.apply(preprocess_text)
-# 			progress_bar.progress(data_count * 0.5)
-# 			df['sentiment'] = df['text_clean'].swifter.apply(get_prediction)
-# 			progress_bar.progress(100)
-# 			st.write(df[[column, 'sentiment']])
-		
-# 		st.balloons()
-
-# 		st.download_button(
-# 			label='Download Result',
-# 			data=df.to_csv(index=False).encode('utf-8'),
-# 			file_name=f'{file.name}-result.csv',
-# 			mime='text/csv'
-# 		)
+with st.expander('Upload File (CSV)'):
+	file = st.file_uploader('Upload File', type=['csv'])
+	delim = st.text_input('Delimiter', value=',', max_chars=1)
+	column = st.text_input('Column Name', value='text')
+	if st.button('Analyze', key='upload'):
+		handle_uploaded_file(st, file, delim, column)
--- shared.py
+++ shared.py
@@ -3,8 +3,16 @@ import pandas as pd
 import re
 import string
 import numpy as np
+import os
+import pickle as pkl
 
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from collections import Counter
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+
+def get_filepath(filename):
+	return os.path.join(root_path, filename)
 
 def case_folding(text):
 	return text.lower()
@@ -18,6 +26,21 @@ def remove_repetitive_char(tokens):
 		else:
 			result.append(re.sub(r'(.)\1{1,}', r'\1', token))
 	return result
+
+def load_term_dict():
+	term_dict_path = get_filepath('dataset/processed/processed_terms_dict.pkl')
+	try:
+		with open(term_dict_path, 'rb') as f:
+			term_dict = pkl.load(f)
+		print(f"Term dictionary: {len(term_dict)} loaded")
+		return term_dict
+	except FileNotFoundError:
+		return {}
+
+def save_term_dict(term_dict):
+	term_dict_path = get_filepath('dataset/processed/processed_terms_dict.pkl')
+	with open(term_dict_path, 'wb') as f:
+		pkl.dump(term_dict, f)
 
 def clean_tweet(tweet):
 	# Remove @mentions
@@ -47,7 +70,7 @@ def tokenize(tweet):
 	return nltk.word_tokenize(tweet)
 
 def load_normalization_list():
-	normalization_list_path = './dataset/wordlist/normalization_list.csv'
+	normalization_list_path = get_filepath('dataset/wordlist/normalization_list.csv')
 	normalization_list = pd.read_csv(normalization_list_path, delimiter=',')
 	list_normalize_targets = list(normalization_list['target'])
 	list_normalize_replacements = list(normalization_list['replacement'])
@@ -55,7 +78,6 @@ def load_normalization_list():
 	return list_normalize_targets, list_normalize_replacements
 
 def normalize(tokens, targets, replacements):
-	return tokens
 	if len(targets) != len(replacements):
 		raise ValueError('Targets and replacements must have the same length')
 	result = []
@@ -74,7 +96,7 @@ def load_stopwords_list():
 	custom_stopwords_list = ['aaaaaaaaa', 'aaaaaaaaaah', 'aaaahhh', 'aaahhh', 'aah', 'aatu', 'ngaoahahahhahahaha',
 													'wkwkwk', 'slebew', 'sih', 'nih', 'deh', 'nya', 'mah', 'breaking', 'news', 'wkwkw']
 
-	lexicon_all = pd.read_csv('./dataset/wordlist/lexicon_dict_all.csv', delimiter=',')
+	lexicon_all = pd.read_csv(get_filepath('dataset/wordlist/lexicon_dict_all.csv'), delimiter=',')
 	lexicon_all = list(lexicon_all['word'])
 
 	indonesian_stopwords_list = [word for word in indonesian_stopwords_list if word not in lexicon_all]
@@ -89,43 +111,86 @@ def load_stopwords_list():
 def remove_stopwords(tokens, stopwords):
 	return [token for token in tokens if token not in stopwords]
 
-def remove_token_with_recurring_characters(tokens):
-	return [token for token in tokens if not re.search(r'(.)\1{3,}', token)]
-
 def build_stemmer():
 	stem_factory = StemmerFactory()
 	stemmer = stem_factory.create_stemmer()
 	return stemmer
 
 stemmer = build_stemmer()
-def stemming(tokens_bag):
-	terms_dict = {}
-	for tokens in tokens_bag:
-		for token in tokens:
-			if token not in terms_dict:
-				terms_dict[token] = ''
-
-	print(f"Unique terms: {len(terms_dict)}")
-
-	for i, term in enumerate(terms_dict):
-		terms_dict[term] = stemmer.stem(f'{term}')
-		if i % 1000 == 0:
-			print(f"On processing... {i} terms have been stemmed")
-	return terms_dict
-
+terms_dict = load_term_dict()
 def stemming_text(text):
-	return stemmer.stem(text)
+	return stemmer.stem(f'{text}')
+def do_stemming(tokens):
+	return [terms_dict[token] for token in tokens]
+def prepare_stemming(tokens):
+	for token in tokens:
+		if token not in terms_dict:
+			terms_dict[token] = stemming_text(text=token)
+	return tokens
+def stemming(df, column='tokens'):
+	print('Stemming...')
+	df[column] = df[column].swifter.apply(prepare_stemming)
+	df[column] = df[column].swifter.apply(do_stemming)
+	df[column] = df[column].swifter.apply(remove_stopwords, args=(list_stopwords,))
+	df['review'] = df[column].swifter.apply(untokenizer)
 
-def preprocess_text(text):
-	stemmer = build_stemmer()
+	save_term_dict(term_dict=terms_dict)
+	return df
 
-	text = case_folding(text)
-	text = clean_tweet(text)
-	tokens = tokenize(text)
-	list_normalize_targets, list_normalize_replacements = load_normalization_list()
-	tokens = normalize(tokens, list_normalize_targets, list_normalize_replacements)
-	list_stopwords = load_stopwords_list()
-	tokens = remove_stopwords(tokens, list_stopwords)
-	tokens = [stemmer.stem(token) for token in tokens]
-	tokens = remove_token_with_recurring_characters(tokens)
+def clean_data_with_rare_word(df, column='tokens', threshold=2):
+	print('Cleaning data with rare word...')
+	print(f"Before: {df.shape}")
+	token_counts = Counter([token for tokens in df[column] for token in tokens])
+	filtered_words = [token for token, count in token_counts.items() if count >= threshold]
+	df = df[df[column].apply(lambda x: any(token in filtered_words for token in x))]
+	df = df.reset_index(drop=True)
+	print(f"After: {df.shape}")
+	return df
+
+def clean_duplicate_data(df, column='tokens'):
+	print('Cleaning duplicate data...')
+	print(f"Before: {df.shape}")
+	df = df.drop_duplicates(subset=column, keep="first")
+	df = df.dropna(subset=[column])
+	df = df[df[column].map(len) > 2]
+	print(f"After: {df.shape}")
+	return df
+
+def clean_data(df, column='tokens',threshold=2):
+	df = clean_data_with_rare_word(df, column, threshold)
+	df = clean_duplicate_data(df, column)
+	df[column] = df[column].swifter.apply(remove_repetitive_char)
+	return df
+
+def untokenizer(tokens):
 	return ' '.join(tokens)
+
+list_normalize_targets, list_normalize_replacements = load_normalization_list()
+list_stopwords = load_stopwords_list()
+def preprocessing_dataframe(df, column='content'):
+	df['text_clean'] = df[column].swifter.apply(clean_tweet)
+	df['case_folding'] = df['text_clean'].swifter.apply(case_folding)
+	df['tokens'] = df['case_folding'].swifter.apply(tokenize)
+	df['review'] = df['tokens'].swifter.apply(untokenizer)
+	return df
+
+def preprocessing_text(text):
+	text = clean_tweet(text)
+	text = case_folding(text)
+	tokens = tokenize(text)
+	tokens = normalize(tokens, list_normalize_targets, list_normalize_replacements)
+	tokens = remove_stopwords(tokens, list_stopwords)
+	tokens = remove_repetitive_char(tokens)
+	tokens = [stemmer.stem(token) for token in tokens]
+	return ' '.join(tokens)
+
+def preprocessing(df_or_text, column=None):
+	if isinstance(df_or_text, pd.DataFrame):
+		return preprocessing_dataframe(df_or_text, column)
+	return preprocessing_text(df_or_text)
+
+def normalizer(df, column='tokens'):
+	df[column] = df[column].swifter.apply(normalize, args=(list_normalize_targets, list_normalize_replacements))
+	df[column] = df[column].swifter.apply(remove_stopwords, args=(list_stopwords,))
+	df['review'] = df[column].swifter.apply(untokenizer)
+	return df
